9cb06c0d472a1f6158bdd8a0ffb2805d9162b335
orbenor <orbenor@gmail.com>
2021-07-25 12:11:05 +0300

    Delete pro-loop.sh

--- pro-loop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for d in /storage/*/; do
-	((printf "$d,";du -sm "$d" | awk '{print $1/1024}';find "$d" -name "*" -type f -exec stat --format '%y' {} \; | cut -d- -f 1 | sort -n | uniq| sed -n '1p;$p')| tr '\n' ',' | tr '\n' ' ')|sed 's/.$//'; echo "" &
-done
